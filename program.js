3694c108a37f5f90dc7cbc19f05fe429e6754d34
Isabela Velandia <velandiasrp30@gmail.com>
2025-02-28 19:01:24 -0500

    version2

---
+++ program.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.613622147268676, -74.15558080896226], 16);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
