db88fdc8b9ffd92c423edf74c1a59be5c8018b85
Isabela Velandia <velandiasrp30@gmail.com>
2025-02-28 19:58:52 -0500

    version3

--- program.js
+++ program.js
@@ -4,3 +4,20 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+async function loadPolygon (){
+    let myData = await fetch('timiza.geojson');
+    let myPolygon = await myData.json();
+    L.geoJSON(myPolygon, 
+        {
+            style: {
+                color: 'blue'
+            }
+        }
+    ).addTo(map);
+}
+
+loadPolygon();
+
+let btnTrees = document.getElementById('btnTrees');
+btnTrees.addEventListener('click', ()=>alert("Hola"));
