fbe9a6ffa0b2086c9f18bae65502b5755ffbf414
Isabela Velandia <velandiasrp30@gmail.com>
2025-03-02 14:04:00 -0500

    version8

--- program.js
+++ program.js
@@ -41,5 +41,90 @@ btnTrees.addEventListener('click',
 
                 }
             }
-        ).addTo(map)
-    })
+        ).addTo(map);
+    }) 
+
+let btnDistance = document.getElementById("btnDistance");
+btnDistance.addEventListener('click', 
+        async ()=> {
+            let response= await fetch("arboles_timiza.geojson");
+            let datos=await response.json();
+            //Agregar la capa al mapa
+            let trees= datos.features.map((myElement, index)=>({
+                id: index+1,
+                coordinates: myElement.geometry.coordinates
+
+            }));
+
+            let distances =[];
+            trees.forEach((treeA) =>{ trees.forEach(
+                    (treeB)=>{
+                        //calcular la distancias de treeA a cada uno de los treeB
+                    if (treeA.id != treeB.id){
+                        let distance = turf.distance(
+                            turf.point(treeA.coordinates),
+                            turf.point(treeB.coordinates),
+                        );
+                        distances.push (
+                            [
+                                `Árbol ${treeA.id}`,
+                                `Árbol ${treeB.id}`,
+                                distance.toFixed(3)
+                            ]
+                        )
+
+                    }
+                }
+                )
+
+                
+            });
+
+            generatePDF(distances, trees.lenght)
+           
+        }
+
+    )
+
+ function generatePDF (distances, totalTrees){
+            let {jsPDF} = window.jspdf;
+            let documentPDF= new jsPDF();
+
+            documentPDF.text("REPORTE DE ÁRBOLES EN EL BARRIO TIMIZA", 10,10)
+            
+            documentPDF.autoTable (
+                {
+                head: [['Árbol 1', 'Árbol 2', 'Distance']],
+                body: distances
+                }
+            );
+            documentPDF.save("timiza.pdf")
+}
+
+let btnIncidentes = document.getElementById("btnIncidentes");
+
+btnIncidentes.addEventListener('click', 
+    async function(){
+        let response = await fetch("siniestros_timiza.geojson");
+        let datos = (await response.json());
+        //Agregar la capa al mapa
+        L.geoJSON(
+            datos,
+            {
+                pointToLayer: (feature, latlong)=>{
+
+                    return L.circleMarker(latlong, {
+                        radius:3,
+                        fillColor:'red',
+                        weight:1,
+                        opacity:0,
+                        fillOpacity: 0.5,
+                    })
+
+                }
+            }
+        ).addTo(map);
+
+    }
+    
+)
